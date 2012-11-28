33e0600c9fde5590ff110609e426fb6b6456d37b
Adrian Petrescu <adrian.petrescu@maluuba.com>
2012-11-28 14:25:33 -0500

    Moving test cases to external files

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -13,23 +13,15 @@ class TestSetup(object):
 
 
 class TestBaseMixin(object):
-    pass
+
+    def read_case(self, name):
+        return file('tests/cases/%s.idl' % name, 'r').read()
 
 
 class ParserTestSuite(TestSetup, TestBaseMixin, unittest.TestCase):
+
     def test_simple_enum(self):
-        ast = parser.parse('''
-enum Subgroup {
-    MUSIC_SONG_REMOVE,
-    MUSIC_SONG_INFO,
-    MUSIC_SONG_PAUSE,
-    MUSIC_RADIO,
-    MUSIC_SONG_RATING,
-    MUSIC_SONG_RESET,
-    MUSIC_SONG_RESUME,
-    MUSIC_SONG_PLAY
-}
-            ''')
+        ast = parser.parse(self.read_case('simple_enum'))
         self.assertEquals(1, len(ast))
         enum_parse = ast[0]
         self.assertEquals('enum', enum_parse[0])
@@ -46,18 +38,11 @@ enum Subgroup {
             ]), set(enum_parse[2]))
 
     def test_simple_struct_with_namespace(self):
-        ast = parser.parse('''
-namespace java org.maluuba.service.MusicService
-
-struct RequestObject {
-    1: string someRequest,
-    2: double someValue
-}
-              ''')
+        ast = parser.parse(self.read_case('simple_struct_with_namespace'))
         self.assertEquals(2, len(ast))
 
         namespace_parse = ast[0]
-        self.assertEquals(('namespace', 'org.maluuba.service.MusicService'), namespace_parse)
+        self.assertEquals(('namespace', 'pyidl.example'), namespace_parse)
 
         struct_parse = ast[1]
         self.assertEquals('struct', struct_parse[0])
@@ -66,44 +51,5 @@ struct RequestObject {
         self.assertEquals(('someValue', 2, 'double'), struct_parse[2][1])
 
     def test_complex_struct(self):
-        ast = parser.parse('''
-namespace cpp thrift.example
-namespace java thrift.example
-
-enum TweetType {
-    TWEET,
-    RETWEET = 2,
-    DM = 0xa,
-    REPLY
-}
-
-struct Location {
-    1: required double latitude;
-    2: required double longitude;
-}
-
-struct Tweet {
-    1: required i32 userId;
-    2: required string userName;
-    3: required string text;
-    4: optional Location loc;
-    5: optional TweetType tweetType = TweetType.TWEET;
-    16: optional string language = "english";
-}
-
-typedef list<Tweet> TweetList
-
-struct TweetSearchResult {
-    1: TweetList tweets;
-}
-
-const i32 MAX_RESULTS = 100;
-
-service Twitter {
-    void ping(),
-    bool postTweet(1:Tweet tweet);
-    TweetSearchResult searchTweets(1:string query);
-    oneway void zip()
-}
-            ''')
+        ast = parser.parse(self.read_case('complex_struct'))
         print ast
